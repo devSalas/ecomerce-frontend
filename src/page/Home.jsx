b27898155b6b29a9c8f58376a0e01bb151e82d78
Gerson Garayar <garayargerson20@gmail.com>
2023-01-09 19:47:47 -0500

    creando carrito de compras

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Buscador from "../components/Buscador";
 import CategorySlider from "../components/CategorySlider";
+import Nav from "../components/Nav";
 import View from "../components/View";
 
 const Home = () => {
   return (
     <div className="max-w-7xl m-auto">
       <h1>hola mundo</h1>
+      <Nav />
       <div className="px-8 py-2">
         <Buscador />
       </div>
